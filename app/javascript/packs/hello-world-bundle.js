c2d3ac0281dbdc6ac778b2a9c9d0ed07de922f0b
Fajarullah <frullah12@gmail.com>
2021-12-11 22:34:06 +0700

    fix: add missing react dependencies

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,5 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
+// TODO: fix cannot resolve '../bundles/HelloWorld/components/HelloWorld'
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
